78e711392a1670c534fc6cc51351cdf1e2ba3e41
CesarBeltran <cabeltran@softcaribbean.com.co>
2022-11-15 15:21:28 -0500

    inicio

---
+++ src/main/java/com/examplelogin/demologinbackend/DemologinbackendApplication.java
@@ -0,0 +1,13 @@
+package com.examplelogin.demologinbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemologinbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemologinbackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/examplelogin/demologinbackend/DemologinbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examplelogin.demologinbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemologinbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
